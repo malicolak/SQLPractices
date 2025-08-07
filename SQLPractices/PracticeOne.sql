8fe85a5b4003e69acfafdb864cb8289873c79c8a
Muhammet Ali Colak <m.alicolak01@hotmail.com>
2025-08-07 15:26:15 +0300

    Update PracticeOne.sql

--- SQLPractices/PracticeOne.sql
+++ SQLPractices/PracticeOne.sql
@@ -2,10 +2,11 @@
 select title, description from film
 --film tablosundaki film uzunluğu altmıştan büyük ve yetmiş beşten küçük olan filmlerin tüm bilgilerini getirir.
 select * from film where length > 60 and length < 75
---film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralar.
+--film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla getirir.
 select * from film where rental_rate = 0.99 and (replacement_cost = 12.99 or replacement_cost = 28.99)
 --customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değerini getirir.
 select last_name from customer where first_name = 'Mary'
 --Sorgu sonucu: "Smith"
---film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralar.
+--film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri getirir.
 select * from film where not length > 50 and (rental_rate != 2.99 or rental_rate != 4.99)
+
